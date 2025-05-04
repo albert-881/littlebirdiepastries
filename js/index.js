dc7cd9673fcaf91aa1146a7fd180b86a29fc8a6c
albert-881 <quinteroalberto88@gmail.com>
2025-05-03 20:58:04 -0500

    test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 const cookiebutton = document.querySelector('#cookie-btn');
+const cupcakebutton = document.querySelector('#cupcake-btn');
+const cakebutton = document.querySelector('#cake-btn');
 
 cookiebutton.addEventListener('click', (e) => {
     console.log('works');
-    window.location.href = 'cookiesMenu.html';
+    window.location.href = 'Menu.html';
+});
+
+cupcakebutton.addEventListener('click', (e) => {
+    console.log('works');
+    window.location.href = 'Menu.html#cupcake-section';
+});
+
+cakebutton.addEventListener('click', (e) => {
+    console.log('works');
+    window.location.href = 'Menu.html#cake-section';
 });

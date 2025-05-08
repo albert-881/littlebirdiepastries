2492400ff7654185036193720d55efa33705b153
albert-881 <quinteroalberto88@gmail.com>
2025-05-08 11:49:43 -0500

    nav menu test

--- js/index.js
+++ js/index.js
@@ -17,3 +17,19 @@ cakebutton.addEventListener('click', (e) => {
     window.location.href = 'Menu.html#cake-section';
 });
 
+
+
+// Initialize Feather icons
+feather.replace();
+
+// Toggle the navigation menu when the hamburger icon is clicked
+document.getElementById('hamburger-icon').addEventListener('click', function () {
+  const menu = document.getElementById('nav-menu');
+  menu.classList.toggle('visible');
+});
+
+// Hide the navigation menu when the arrow (close button) is clicked
+document.getElementById('close-btn').addEventListener('click', function () {
+    const menu = document.getElementById('nav-menu');
+    menu.classList.toggle('visible'); // Hide the menu
+  });
